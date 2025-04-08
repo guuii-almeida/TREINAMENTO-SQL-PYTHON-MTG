d457ac87c6ff0f5b9af195f458fcdb13659193a8
Guilherme <guuiilhermealmeida@gmail.com>
2025-04-08 19:31:40 -0300

    Iniciando view e JS de busca

--- portalWeb/views.py
+++ portalWeb/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import requests
+from time import sleep
 # Create your views here.
 
 
 def index(request):
     return render(request,'index.html')
 
+
+def searchCard(request):
+    
+    return JsonResponse
+
 def buildDeck(request):
+    
     return render(request,'buildDeck.html')
