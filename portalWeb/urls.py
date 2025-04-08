c95f88e2f6dd3f146f3b480c1ce7af73c2710224
Guilherme <guuiilhermealmeida@gmail.com>
2025-04-08 20:25:43 -0300

    Config inicial do buscador de cartas

--- portalWeb/urls.py
+++ portalWeb/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path('buildDeck', views.buildDeck, name='buildDeck'),
+   path('searchCard/<str:cardInput>/', views.searchCard, name='searchCard'),
 ]
--- portalWeb/views.py
+++ portalWeb/views.py
@@ -9,9 +9,19 @@ def index(request):
     return render(request,'index.html')
 
 
-def searchCard(request):
-    
-    return JsonResponse
+def searchCard(request, cardInput):
+    sleep(0.5)
+    try:
+        res = requests.get(f'https://api.scryfall.com/cards/search?q={cardInput}')
+        dataCard = res.json()
+
+        cardNamesList = [card['name'] for card in dataCard['data'][:5]]
+        print(cardNamesList)
+        return JsonResponse({
+            'cardNamesList': cardNamesList,
+        })
+    except Exception as e:
+        return JsonResponse({'error': 'Not a single card was found!'}, status=400)
 
 def buildDeck(request):
     
