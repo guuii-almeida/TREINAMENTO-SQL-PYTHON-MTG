db6ebb7f559861540111f20889383a830eaae772
Guilherme <guuiilhermealmeida@gmail.com>
2025-04-09 20:55:46 -0300

    cardResult atualizado e adicao de selectCard

--- portalWeb/static/js/searchCard.js
+++ portalWeb/static/js/searchCard.js
@@ -1,33 +1,33 @@
-document.getElementById('card-input').addEventListener('input', function () {
-    const cardName = this.value.trim();
-    const cardResult = document.getElementById('div-card-result');
+// document.getElementById('card-input').addEventListener('input', function () {
+//     const cardName = this.value.trim();
+//     const cardResult = document.getElementById('div-card-result');
 
-    if (cardName.length > 2) {
+//     if (cardName.length > 2) {
 
-        fetch(`/searchCard/${encodeURIComponent(cardName)}/`)
-            .then(response => response.json())
-            .then(data => {
-                cardResult.innerHTML = '';
+//         fetch(`/searchCard/${encodeURIComponent(cardName)}/`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 cardResult.innerHTML = '';
 
-                if (data.error) {
-                    cardResult.innerHTML = `<p style="color:red;">${data.error}</p>`;
-                } else if (data.cardNamesList && data.cardNamesList.length > 0) {
-                    cardResult.removeAttribute('hidden');
-                    const ul = document.createElement('ul');
-                    data.cardNamesList.forEach(name => {
-                        const li = document.createElement('li');
-                        li.textContent = name;
-                        ul.appendChild(li);
-                    });
-                    cardResult.appendChild(ul);
-                } else {
-                    cardResult.removeAttribute('hidden');
-                    cardResult.innerHTML = `<p>Not a single card was found!</p>`;
-                }
-            })
+//                 if (data.error) {
+//                     cardResult.innerHTML = `<p style="color:red;">${data.error}</p>`;
+//                 } else if (data.cardNamesList && data.cardNamesList.length > 0) {
+//                     cardResult.removeAttribute('hidden');
+//                     // const ul = document.createElement('ul');
+//                     data.cardNamesList.forEach(name => {
+//                         const a = document.createElement('a');
+//                         a.textContent = name;
+//                         cardResult.appendChild(a);
+//                     });
+//                     // cardResult.appendChild(ul);
+//                 } else {
+//                     cardResult.removeAttribute('hidden');
+//                     cardResult.innerHTML = `<p>Not a single card was found!</p>`;
+//                 }
+//             })
 
-    } else {
-        document.getElementById('div-card-result').innerHTML = '';
-        cardResult.setAttribute('hidden');
-    }
-});
+//     } else {
+//         document.getElementById('div-card-result').innerHTML = '';
+//         cardResult.setAttribute('hidden');
+//     }
+// });
